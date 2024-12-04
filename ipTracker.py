d506b317714e934d8a989bafbd1abbe7fa4b37d5
alvvos <alvaro.salis@gmail.com>
2024-12-04 17:56:29 +0100

    Perfeccionamiento del iptracker

--- ipTracker.py
+++ ipTracker.py
@@ -1,26 +1,46 @@
 from flask import Flask, request, redirect
 import datetime
+import requests
 
 app = Flask(__name__)
 
 LOG_FILE = 'ip_logs.txt'
+IPINFO_API_KEY = 'b63f20e2505b2e'  
+
+def get_public_ip():
+    
+    public_ip = request.remote_addr
+    if public_ip == '127.0.0.1':
+        response = requests.get('https://api.ipify.org?format=json')
+        if response.status_code == 200:
+            public_ip = response.json()['ip']
+    return public_ip
 
 @app.route('/')
-def home():
-    return "Rastreador de IPs. Usa /generate para generar un nuevo enlace de registro."
-@app.route('/generate')
-def generate():
-    unique_id = "track"
-    tracking_link = f"http://127.0.0.1:5000/{unique_id}"
-    return f"Tu enlace rastreable es : {tracking_link}"
-@app.route('/track')
 def track():
-    ip_adress = request.remote_addr
+    ip_address = get_public_ip()
     user_agent = request.headers.get('User-Agent')
+    referer = request.headers.get('Referer', 'None')
+    method = request.method
     timestamp = datetime.datetime.now().isoformat()
 
+    geo_info = get_geolocation(ip_address)
+    if geo_info:
+        city = geo_info.get('city', 'N/A')
+        region = geo_info.get('region', 'N/A')
+        country = geo_info.get('country', 'N/A')
+    else:
+        city = region = country = 'N/A'
     with open(LOG_FILE, 'a') as f:
-        f.write(f"{ip_adress},{user_agent},{timestamp}\n")
+        f.write(f"{ip_address},{user_agent},{referer},{method},{timestamp},{city},{region},{country}\n")
+
     return redirect('https://youtube.com')
+
+def get_geolocation(ip_address):
+    response = requests.get(f'https://ipinfo.io/{ip_address}/json?token={IPINFO_API_KEY}')
+    if response.status_code == 200:
+        return response.json()
+    return None
+
 if __name__ == '__main__':
     app.run(debug=True)
